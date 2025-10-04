e2876c9e4f94ac7cd77653efd1afe2108eae4e83
1008pawan <1008pawansharma@gmail.com>
2025-10-04 14:54:35 +0530

    afternoon change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import HeroSection from '@/components/HeroSection'
+import OurServices from '@/components/OurServices'
 import SecondHeroSection from '@/components/SecondHeroSection'
 import React from 'react'
 
@@ -7,6 +8,7 @@ const page = () => {
     <div>
         <HeroSection />
         <SecondHeroSection />
+        <OurServices />
     </div>
   )
 }
