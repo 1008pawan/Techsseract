5a5f82327dadd5dc8803fbca78b780a2368b7a45
1008pawan <1008pawansharma@gmail.com>
2025-10-09 10:45:14 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,66 @@ import React, { useState } from "react";
 import Button from "./Button";
 import SmartHomeLink from "./SmartHomeLink";
 
+const Gallery = [
+  {
+    image: (
+      <img
+        src="images/footer/service-img-1.jpg"
+        alt="Gallery 1"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="images/footer/service-img-2.jpg"
+        alt="Gallery 2"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="images/footer/service-img-3.jpg"
+        alt="Gallery 3"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="images/footer/service-img-4.jpg"
+        alt="Gallery 4"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="images/footer/service-img-5.jpg"
+        alt="Gallery 5"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="images/footer/service-img-6.jpg"
+        alt="Gallery 6"
+        className="w-full h-full object-cover rounded"
+      />
+    ),
+  },
+];
+
 const Footer = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <footer className=" text-white">
@@ -99,38 +157,44 @@ const Footer = () => {
         <div>
           <h3 className="text-lg font-semibold mb-3">Our Gallery</h3>
           <div className="grid grid-cols-2 gap-2">
-            <img
-              src="images/footer/service-img-1.jpg"
-              alt="Gallery 1"
-              className="w-full h-15 object-cover rounded"
-            />
-            <img
-              src="images/footer/service-img-2.jpg"
-              alt="Gallery 2"
-              className="w-full h-15 object-cover rounded"
-            />
-            <img
-              src="images/footer/service-img-3.jpg"
-              alt="Gallery 3"
-              className="w-full h-15 object-cover rounded"
-            />
-            <img
-              src="images/footer/service-img-4.jpg"
-              alt="Gallery 4"
-              className="w-full h-15 object-cover rounded"
-            />
-            <img
-              src="images/footer/service-img-5.jpg"
-              alt="Gallery 5"
-              className="w-full h-15 object-cover rounded"
-            />
-            <img
-              src="images/footer/service-img-6.jpg"
-              alt="Gallery 6"
-              className="w-full h-15 object-cover rounded"
-            />
+            {Gallery.map((img, index) => (
+              <div key={index} onClick={() => setSelectedImage(img.image)}>
+                {img.image}
+              </div>
+            ))}
           </div>
         </div>
+
+        {selectedImage && (
+          <div className="fixed inset-0 z-50 overflow-auto bg-black/80 flex justify-center items-center">
+            <div className="relative md:w-[70%] w-[90%] md:h-140 h-[70%] overflow-y-auto rounded-2xl bg-black">
+              <button
+                onClick={() => {
+                  setSelectedImage(null);
+                }}
+                className="absolute top-4 right-4 text-black cursor-pointer hover:text-blue-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+              <div className="w-full h-full flex justify-center items-center">
+                {selectedImage}
+              </div>
+            </div>
+          </div>
+        )}
 
         {/* Contact Us */}
         <div>
@@ -186,46 +250,45 @@ const Footer = () => {
           >
             <Button btnText={"View On Map"} />
           </div>
+          {isModalOpen && (
+            <div className="fixed inset-0 z-50 overflow-auto bg-black/80 flex justify-center items-center">
+              <div className="relative md:w-[70%] w-[90%] md:h-140 h-[70%] overflow-y-auto rounded-2xl bg-black">
+                <button
+                  onClick={() => {
+                    setIsModalOpen(false);
+                  }}
+                  className="absolute top-4 right-4 text-white cursor-pointer hover:text-orange-500"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+                <div className="w-full h-full">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3559.787750358177!2d80.98947667543716!3d26.846702376686622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjbCsDUwJzQ4LjEiTiA4MMKwNTknMzEuNCJF!5e0!3m2!1sen!2sin!4v1759929072259!5m2!1sen!2sin"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    className="w-full h-full rounded"
+                  />
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
-      {isModalOpen && (
-        <div className="fixed inset-0 z-50 overflow-auto bg-black/80 flex justify-center items-center">
-          <div className="relative md:w-[70%] w-[90%] md:h-140 h-[70%] overflow-y-auto rounded-2xl bg-black">
-            <button
-              onClick={() => {
-                setIsModalOpen(false);
-              }}
-              className="absolute top-4 right-4 text-white cursor-pointer hover:text-orange-500"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-            <div className="w-full h-full">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3559.787750358177!2d80.98947667543716!3d26.846702376686622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjbCsDUwJzQ4LjEiTiA4MMKwNTknMzEuNCJF!5e0!3m2!1sen!2sin!4v1759929072259!5m2!1sen!2sin"
-                style={{ border: 0 }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                className="w-full h-full rounded"
-              />
-            </div>
-          </div>
-        </div>
-      )}
 
       {/* Footer Bottom */}
       <div className="bg-blue-600 flex flex-col md:flex-row justify-between items-center p-6 text-sm text-white">
